c563c036d1bb505e4cf2a0a6f5c63acccbba91a8
nhuong <nhuong>
2019-07-25 22:45:04 +0700

    Basic update

--- App.js
+++ App.js
@@ -3,23 +3,6 @@ import { Text, View, StyleSheet, FlatList } from 'react-native';
 import Constants from 'expo-constants';
 import FlatListItems from './components/flatListItems';
 
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>
-          Top Ten Movies of IMDB
-        </Text>
-        <FlatListItems />
-      </View>
-    );
-  }
-}import * as React from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
-import Constants from 'expo-constants';
-import FlatListItems from './components/flatListItems';
-
 export default class App extends React.Component {
   render() {
     return (
@@ -41,23 +24,6 @@ const styles = StyleSheet.create({
   },
   paragraph: {
     marginVertical: 20,
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: 'white',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    marginBottom: 12,
     fontSize: 18,
     textAlign: 'center',
   },
